7b62fc0351186d869069bf79f8e785142e1e15f9
Vielhaber <sales@vcta.at>
2026-09-03 16:29:30 +0200

    Delete src/src directory

--- src/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/** shadcn/ui's standard classname combinator: merges Tailwind classes and resolves conflicts. */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
